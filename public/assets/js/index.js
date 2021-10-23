0463a7c75d8f3bd683cf35ef125765c3ebd6ad07
Matt545 <84997670+Matt545@users.noreply.github.com>
2021-10-23 01:26:15 -0700

    trying to push new note to array

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -39,8 +39,18 @@ const saveNote = (note) =>
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(note),
-  });
+    body: JSON.stringify(note)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+      .then(postResponse => {
+        console.log(postResponse);
+        alert('Added Note');
+      });
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
+const { createNewNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -8,5 +9,13 @@ router.get('/notes', (req, res) => {
 
     //res.send(results);
 });
+
+router.post('/notes', (req, res) => {
+    //create an id for the note
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(req.body);
+})
 
 module.exports = router;
